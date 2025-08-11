5e5ad8b8e20cb893187b4c6a01c5d2af5b3a95d4
dosagod <pashin894@gmail.com>
2025-08-10 18:34:32 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,7 @@ import heroImage from "@/assets/hero-feast.jpg";
 
 const Hero = () => {
   return (
-    <section className="relative min-h-screen flex items-center justify-center overflow-hidden pt-16">{/* Added pt-16 for header space */}
-
-      image={hero-feast}    
+    <section className="relative min-h-screen flex items-center justify-center overflow-hidden pt-16">{/* Added pt-16 for header space */}    
       
       {/* Content */}
       <div className="relative z-10 text-center text-white px-4 sm:px-6 lg:px-8 max-w-4xl mx-auto">
