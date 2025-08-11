b346dfb027db3cffb375f795b232d1d8b65d215d
dosagod <pashin894@gmail.com>
2025-08-10 18:55:22 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import heroImage from "@/assets/hero-feast.jpg";
 
 const Hero = () => {
   return (
-    <section style="background-image: {hero-feast}; background-size: cover; background-repeat: no-repeat; background-position: center center; height: 300px; width: 100%;">   
+    <section style="background-image: {{hero-feast}}; background-size: cover; background-repeat: no-repeat; background-position: center center; height: 300px; width: 100%;">   
 
       {/* Content */}
       <div className="relative z-10 text-center text-white px-4 sm:px-6 lg:px-8 max-w-4xl mx-auto">
