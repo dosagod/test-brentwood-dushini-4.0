c31f029bd8b9db8de5927707fbe3a8acb96ff38a
dosagod <pashin894@gmail.com>
2025-08-10 18:31:09 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import heroImage from "@src/assets/hero-feast.jpg";
+import heroImage from "@/assets/hero-feast.jpg";
 
 const Hero = () => {
   return (
